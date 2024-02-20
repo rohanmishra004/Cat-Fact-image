7f6c8e8a7ace784fe6590d0ddff8afc165b97adf
Rohan Mishra <rohanmishra.004@gmail.com>
2024-02-21 00:21:48 +0530

    Random Cat Fact and Image Generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios'
 
 function App() {
+  const [data, setData] = useState('')
+  const [image, setCatImage] = useState('')
+  const handleFetch = () => {
+    // fetch('https://catfact.ninja/fact')
+    //   .then((res) => res.json())
+    //   .then(data => setData(data.fact))
+    //   .catch((err) => console.log(err))
+    Axios.get('https://catfact.ninja/fact').then((res) => {
+      setData(res.data.fact)
+    })
+
+    Axios.get('https://api.thecatapi.com/v1/images/search').then((res) => {
+      setCatImage(res.data[0].url)
+    })
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleFetch}>Generate Cat fact</button>
+
+      <div >
+        <div>
+          {data && <h1>{data}</h1>}
+
+          {image && <img src={image} alt='cats' />}
+        </div>
+
+      </div>
+
+
     </div>
   );
 }
